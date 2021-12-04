b60aa2b0dd661f471902748bda204a4e875803ac
KIRAN KUMAR G <84234017+kirankumarcr14@users.noreply.github.com>
2021-12-04 15:04:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,95 @@
+
+    let myResume={
+        "Basics": {
+          "Name": "Mr.Kiran Kumar G",
+          "Outlook" : "kirankiumarg7100@outlook.com",
+          "Email": "kirankumarcr14@gmail.com",
+          "Phone": "+91 78923 58431 / +91 92068 87577",
+          "Degree": "B.E computer science",
+          "Location": {
+            "Address": "#284 , 9th Main road ,3rd stage,1st Block ,West of chord Road",
+            "Area" : "Manjunathnagar",
+            "PostalCode": "560010",
+            "Sity": "Bangalore",
+            "State": "Karnataka",
+            "Country": "India"
+          },
+        },
+          "Career objective":
+          {
+             "career"  :"Intend to enforce my knowledge with estemm organistaion and engage with committed and dedicated people and added value to the organistaion"
+          },
+          "Educational Qualification":
+          {
+              "Bachelore of Engineering " :{
+                  "institution" : " Shri Pillappa College of Engineering",
+                  "Board /University ": " Visveswaraya Technologival University",
+                  "Year  of Passing" : "2012- 2016"
+              },
+              "Class 12th " : {
+                "institution" : " St.Ann`s Composite PU College",
+                "Board /University ": " Karanataka PU Board",
+                "Year  of Passing" : "2010-2012"
+
+              },
+              "Class 10th " : {
+                "institution" : " St.Miras High School",
+                "Board /University ": "Karnataka Secondary School Leaving Certifiacte Board",
+                "Year  of Passing" : "2010"
+              },
+          },
+          "Technical Skills" :
+          {
+              "Operating Skills" : " Windows",
+              "Programming Skills" : "C,C++,Java",
+              "Web Developing skills": "HTML",
+              "Database" : "MYSQL"
+
+          },
+          "Work Experience": {
+              "HR  Executive" :
+              {
+                  "Duration" : "May 2017 - nov 2017",
+                  "Work": " Associated with the interview proces for the :Askeixm Private Limted",
+                  " Period" : "7 months"
+              },
+              "Assitant Teacher" : 
+              {
+                  "Duration " : "Nov 2017 to Present",
+                  "work": " Joined as Computer and Mathematics Teacher for the class 7, 8 and 9",
+                  "Period" : " 5 academic year",
+            },
+         
+        },
+        "Area of Strength" :{
+
+        " Strength " :" New challenges and assignmnet , Work in positive metal attitude, work hard honestly , good communication , Tea leader with good motivation and Leadershio quality"
+
+        },
+        "Area of Interest":
+        {
+            "Inteset" : "Acting , Debating, Playing Sudoku,Teaching",
+        },
+        "Bio Data" :
+        {
+            "Name": "Mr.kIran Kumar G",
+            "Date of Birth" :  "06/May /1994",
+            "Fathers Name" : "Ganesh M",
+            "Mothers Name" : "Meena G",
+            "No of Siblings":  "1",
+            "NAtionality" : "Indian",
+            "Gender" : "Male",
+            "Language Known": " English , Tamil , Kannada , Hindi" ,
+
+        },
+        "Decalaration":
+        {
+            "declare": " I hereby declare all the above information is treu according it my knowldege"
+
+        },
+        "Place": " Bangalore, Karnataka",
+        "Date" : "04/12/2021 ",
+        "Signature" : "Kiran kumar G"
+    }
+    
+console.log(myResume);
